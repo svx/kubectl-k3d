b3839ec5e463e7f68794aa3c2fa63bb3ba869b4c
sven <sven@leftxs.org>
2024-03-07 14:13:31 +0100

    wip rename cmds to match default k3d commands

--- cmd/clusterList.go
+++ cmd/clusterList.go
@@ -9,16 +9,22 @@ import (
 	"os/exec"
 
 	"github.com/fatih/color"
+
 	"github.com/spf13/cobra"
 )
 
-// listCmd represents the list command
-var listCmd = &cobra.Command{
+// clusterListCmd represents the clusterList command
+var clusterListCmd = &cobra.Command{
 	Use:   "list",
-	Short: "List of all running k3d clusters",
-	Long:  `List all running k3d cluster.`,
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		//fmt.Println("list called")
+		//fmt.Println("clusterList called")
 		color.Green("List of available k3d cluster")
 		listK3d()
 	},
@@ -33,15 +39,15 @@ func listK3d() {
 }
 
 func init() {
-	rootCmd.AddCommand(listCmd)
+	clusterCmd.AddCommand(clusterListCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// clusterListCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// clusterListCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
